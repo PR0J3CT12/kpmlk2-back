7552940f3eda2bad8ac6c50c014da621ee090cd5
nickrotay <rotaynd@vniir-m.ru>
2023-09-04 13:14:36 +0300

    .

--- kpm/apps/homeworks/views.py
+++ kpm/apps/homeworks/views.py
@@ -50,7 +50,7 @@ def create_homework(request):
         fields = len(answers)
         files = files.getlist('files')
         for file in files:
-            if file.content_type not in ['image/jpeg', 'image/png', 'application/pdf', 'image/heic', 'image/heif']:
+            if (file.content_type == 'application/pdf') or 'image' in file.content_type:
                 return HttpResponse(
                     json.dumps({'state': 'error', 'message': 'Недопустимый файл.', 'details': {},
                                 'instance': request.path},
@@ -207,7 +207,7 @@ def update_homework(request):
         if 'files' in files:
             files = files.getlist('files')
             for file in files:
-                if file.content_type not in ['image/jpeg', 'image/png', 'application/pdf', 'image/heic', 'image/heif']:
+                if (file.content_type == 'application/pdf') or 'image' in file.content_type:
                     return HttpResponse(
                         json.dumps({'state': 'error', 'message': 'Недопустимый файл.', 'details': {},
                                     'instance': request.path},
@@ -455,7 +455,7 @@ def create_response(request):
                     ensure_ascii=False), status=403)
         files = files.getlist('files')
         for file in files:
-            if file.content_type not in ['image/jpeg', 'image/png', 'application/pdf', 'image/heic', 'image/heif']:
+            if (file.content_type == 'application/pdf') or 'image' in file.content_type:
                 return HttpResponse(
                     json.dumps({'state': 'error', 'message': 'Недопустимый файл.', 'details': {},
                                 'instance': request.path},
