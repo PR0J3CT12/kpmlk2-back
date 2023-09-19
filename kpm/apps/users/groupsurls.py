29074279c51fb49a52d4a03a9b01bc46ccebbf62
nickrotay <rotaynd@vniir-m.ru>
2023-09-19 03:47:12 +0300

    .

---
+++ kpm/apps/users/groupsurls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create-group', views.create_group, name='create group'),
+    path('get-groups', views.get_groups, name='get groups'),
+    path('delete-group', views.delete_group, name='delete group'),
+    path('add-to-group', views.add_to_group, name='add to group'),
+    path('delete-from-group', views.delete_from_group, name='delete from group'),
+]
