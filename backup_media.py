36b2317efab307234b3d06627f4c21849f5b4090
nickrotay <rotaynd@vniir-m.ru>
2023-11-21 03:25:20 +0300

    .

--- backup_media.py
+++ backup_media.py
@@ -1,6 +1,5 @@
 import os
 import yadisk
-import dotenv
 from datetime import datetime
 from pathlib import Path
 
@@ -8,20 +7,17 @@ from pathlib import Path
 PROJECT_ROOT = os.path.dirname(__file__)
 DOTENV_PATH = os.path.join(PROJECT_ROOT, '.env')
 BACKUP_PATH = os.path.join(PROJECT_ROOT, '../backups')
-dotenv.load_dotenv(DOTENV_PATH)
+MEDIA_PATH = os.path.join(PROJECT_ROOT, 'media')
 YANDEX_ID = os.environ.get("YANDEX_ID")
 YANDEX_PASSWORD = os.environ.get("YANDEX_PASSWORD")
 YANDEX_TOKEN = os.environ.get("YANDEX_TOKEN")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_USER_PASSWORD = os.environ.get("DB_USER_PASSWORD")
 
 now = datetime.now()
 day = now.day
 month = now.month
 year = now.year
 
-BACKUP_FILE_PATH = f'{BACKUP_PATH}/{day}-{month}-{year}.dump'
+BACKUP_FILE_PATH = f'{BACKUP_PATH}/{day}-{month}-{year}.tar.gz'
 BACKUP_LOG_FILE_PATH = f'{BACKUP_PATH}/logs.txt'
 
 
@@ -35,39 +31,12 @@ def upload_file(file):
         return str(e)
 
 
-def get_url_for_token():
-    """
-    :return: url for yandex oauth
-    """
-    y_ = yadisk.YaDisk(YANDEX_ID, YANDEX_PASSWORD)
-    url_ = y_.get_code_url()
-    return url_
-
-
-def yandex_accept_token(code_):
-    """
-    token creator
-    :param code_: code from yandex accept page
-    :return: token or empty string
-    """
-    y_ = yadisk.YaDisk(YANDEX_ID, YANDEX_PASSWORD)
-    try:
-        response_ = y_.get_token(code_)
-    except yadisk.exceptions.BadRequestError:
-        return "Bad code"
-    y_.token = response_.access_token
-    if y_.check_token():
-        return y_.token
-    else:
-        return "Bad code"
-
-
 def main():
-    os.system(f'pg_dump --dbname=postgresql://{DB_USER}:{DB_USER_PASSWORD}@127.0.0.1:5432/{DB_NAME} > {BACKUP_FILE_PATH}')
+    os.system(f'tar -zcf {BACKUP_FILE_PATH} {MEDIA_PATH}')
     backup_file = Path(f'{BACKUP_FILE_PATH}')
     if backup_file.is_file():
         with open(BACKUP_LOG_FILE_PATH, 'a') as f:
-            f.write(f'{datetime.now()} | backup db V | upload ')
+            f.write(f'{datetime.now()} | backup media V | upload ')
         try:
             upload_file(BACKUP_FILE_PATH)
             with open(BACKUP_LOG_FILE_PATH, 'a') as f:
