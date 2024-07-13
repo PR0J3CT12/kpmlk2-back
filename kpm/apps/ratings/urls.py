d213cbd8554555022a9b11a0787055480e2cd457
nickrotay <rotaynd@vniir-m.ru>
2024-07-13 20:31:57 +0300

    .

--- kpm/apps/ratings/urls.py
+++ kpm/apps/ratings/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('get-ratings', views.get_ratings, name='get ratings'),
     path('get-rating', views.get_rating, name='get rating'),
     path('create-rating', views.create_rating, name='create rating'),
+    path('update-rating', views.update_rating, name='update rating'),
     path('delete-rating', views.delete_rating, name='delete rating'),
     path('delete-from-rating', views.delete_from_rating, name='delete from rating'),
     path('add-to-rating', views.add_to_rating, name='add to rating'),
