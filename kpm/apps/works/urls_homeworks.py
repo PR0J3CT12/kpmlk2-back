46caf6d28c7f22a5e9e76ba51ed2fe5733747808
nickrotay <rotaynd@vniir-m.ru>
2024-07-27 19:41:08 +0300

    .

--- kpm/apps/works/urls_homeworks.py
+++ kpm/apps/works/urls_homeworks.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('get-my-homeworks', views.get_my_homeworks, name='get user homeworks'),
     path('check-user-homework', views.check_user_homework, name='check user homework'),
     path('create-response', views.create_response, name='create response on homework'),
-    path('check-homework', views.check_user_homework, name='check user homework'),
     path('get-user-work', views.get_user_work, name='get user homework'),
     path('add-to-homework', views.add_to_work, name='add to homework'),
     path('delete-from-homework', views.delete_from_work, name='delete user from work'),
