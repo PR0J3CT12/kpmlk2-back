dddff3e414b5456217782078933b8e489c995bfc
nickrotay <rotaynd@vniir-m.ru>
2024-10-04 15:29:50 +0300

    .

--- kpm/jwt.py
+++ kpm/jwt.py
@@ -5,14 +5,14 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if response.status_code != 200:
-        print(response.data['detail'])
-        if response.data['detail'] == 'Учетные данные не были предоставлены.':
+        detail = response.data['detail']
+        if detail == 'Учетные данные не были предоставлены.':
             response.data = {'state': 'error', 'message': f'Пользователь не аутентифицирован.', 'details': {},
                              'instance': context['request'].path}
-        if response.data['detail'] == 'Данный токен недействителен для любого типа токена':
+        if detail == 'Данный токен недействителен для любого типа токена':
             response.data = {'state': 'error', 'message': f'Недействительный токен.', 'details': {},
                              'instance': context['request'].path}
-        if response.data['detail'] == 'У вас недостаточно прав для выполнения данного действия.':
+        if detail == 'У вас недостаточно прав для выполнения данного действия.':
             response.data = {'state': 'error', 'message': f'У вас недостаточно прав для выполнения данного действия.', 'details': {},
                              'instance': context['request'].path}
     return response
